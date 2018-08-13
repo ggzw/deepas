71d919951725b03185f1e192a008c977230ef443
ggzw <42340584+ggzw@users.noreply.github.com>
2018-08-13 18:51:37 +0800

    Update deep_AS_config.py

--- deep_AS_config.py
+++ deep_AS_config.py
@@ -14,16 +14,16 @@ tf.app.flags.DEFINE_string("test_dir",
                            "../data64/splite_site_test_64bp",
                             "Testing directory")
 
-tf.app.flags.DEFINE_integer("vocab_size",
+tf.app.flags.DEFINE_integer("vocab_size",   #碱基类型
                             5,
                             "vocab size the one hot length")
 tf.app.flags.DEFINE_integer("batch_size",
-                            8,
+                            128,
                             "Set the batch size.")
-tf.app.flags.DEFINE_integer("num_units",
+tf.app.flags.DEFINE_integer("num_units",   #隐藏单元数
                             512,
                             "set the hidden size")
-tf.app.flags.DEFINE_integer("seq_len",
+tf.app.flags.DEFINE_integer("seq_len",     #以剪接位点为中心的序列长度，代表一个剪接位点，两个剪接位点代表一个外显子或相邻外显子。
                             64,
                             "the splice site len")
 
@@ -33,25 +33,22 @@ tf.app.flags.DEFINE_integer("epoch",
 tf.app.flags.DEFINE_integer("learning_rate",
                             0.00005,
                             "Set learning rate")
-tf.app.flags.DEFINE_integer("label_class",
-                            4,
+tf.app.flags.DEFINE_integer("label_class",      #预测结果的种类
+                            2,
                             "Set the prediction label class site.")
-tf.app.flags.DEFINE_string("model",
+tf.app.flags.DEFINE_string("model",         #模型选择
                             "train1",
                             "Set model of the main")
-tf.app.flags.DEFINE_boolean("reuse_model",
+tf.app.flags.DEFINE_boolean("reuse_model",     
                             True,
                             "Set to True to reuse a model")
-tf.app.flags.DEFINE_string("model_dir",
+tf.app.flags.DEFINE_string("model_dir",     
                             "../G_bysj/model1",
                             "model dir to save and reload model")
-tf.app.flags.DEFINE_string("rnn_dir",
-                            "../Z_bysj/",
-                            "rnn_dir")
 tf.app.flags.DEFINE_string("log_dir",
                             "../G_bysj/log1",
                             "log dir to save and reload model")
-tf.app.flags.DEFINE_integer("steps_per_checkpoint",
+tf.app.flags.DEFINE_integer("steps_per_checkpoint",   #没训练几步输出一次结果
                             10,
                             "checkpoint")
 tf.app.flags.DEFINE_string("GPU",
@@ -60,7 +57,7 @@ tf.app.flags.DEFINE_string("GPU",
 tf.app.flags.DEFINE_integer("path_class",
                             2,
                             "pathclass")
-tf.app.flags.DEFINE_integer("maxlen",
+tf.app.flags.DEFINE_integer("maxlen",     #转录本含几个外显子*2（剪接位点个数）
                             300,
                             "maxlen")
 tf.app.flags.DEFINE_integer("current_epoch",1,"改变学习率") 
